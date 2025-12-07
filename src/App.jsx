fbe50da7b8b3f1d2a0bdc012697add778b549854
LydiaBondareva <lidiabondareva08@gmail.com>
2025-12-07 21:57:37 +0500

    with yup

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,38 @@ import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+const formSchema = yup.object().shape({
+	email: yup
+		.string()
+		.matches(
+			/^[\w_%+-]+@[\w-]+\.[\w]{2,}$/,
+			`Пожалуйста, введите корректный email-адрес. Убедитесь, что:
+				- он содержит символ "@"
+				- в имени пользователя (до "@") используются только латинские буквы, цифры, точки, подчёркивания, %, + или -
+				- в доменном имени (после "@") используются только латинские буквы, цифры и дефисы
+				- есть хотя бы одна точка (например, example.com)`,
+		)
+		.required('Поле "email" обязательно для заполнения'),
+	password: yup
+		.string()
+		.matches(
+			/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+			`Пожалуйста, введите корректный пароль. Убедитесь, что он содержит:
+				- минимум 8 символов
+				- минимум одну цифру
+				- минимум одну строчную латинскую букву
+				- минимум одну заглавную латинскую букву`,
+		)
+		.required('Поле "пароль" обязательно для заполнения'),
+	passwordCheck: yup
+		.string()
+		.required('Поле "повторите пароль" обязательно для заполнения')
+		.oneOf([yup.ref('password')], 'Пароли не совпадают'),
+});
+
 function App() {
 	const {
 		register,
-		watch,
 		handleSubmit,
 		formState: { errors },
 	} = useForm({
@@ -16,6 +44,7 @@ function App() {
 			password: '',
 			passwordCheck: '',
 		},
+		resolver: yupResolver(formSchema),
 	});
 
 	const registerRef = useRef(null);
@@ -23,36 +52,6 @@ function App() {
 	const emailErr = errors.email?.message;
 	const passwordErr = errors.password?.message;
 	const checkPasswordErr = errors.passwordCheck?.message;
-	const password = watch('password');
-
-	const emailProps = {
-		pattern: {
-			value: /^[\w_%+-]+@[\w-]+\.[\w]{2,}$/,
-			message: `Пожалуйста, введите корректный email-адрес. Убедитесь, что:
-				- он содержит символ "@"
-				- в имени пользователя (до "@") используются только латинские буквы, цифры, точки, подчёркивания, %, + или -
-				- в доменном имени (после "@") используются только латинские буквы, цифры и дефисы
-				- есть хотя бы одна точка (например, example.com)`,
-		},
-		required: 'Поле "email" обязательно для заполнения',
-	};
-
-	const passwordProps = {
-		pattern: {
-			value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
-			message: `Пожалуйста, введите корректный пароль. Убедитесь, что он содержит:
-				- минимум 8 символов
-				- минимум одну цифру
-				- минимум одну строчную латинскую букву
-				- минимум одну заглавную латинскую букву`,
-		},
-		required: 'Поле "пароль" обязательно для заполнения',
-	};
-
-	const passwordCheckProps = {
-		required: 'Поле "повторите пароль" обязательно для заполнения',
-		validate: (value) => value === password || 'Пароли не совпадают',
-	};
 
 	function onSubmit(formData) {
 		if (registerRef.current) {
@@ -69,19 +68,19 @@ function App() {
 			{checkPasswordErr && <div className={styles.error}>{checkPasswordErr}</div>}
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<input
-					{...register('email', emailProps)}
+					{...register('email')}
 					type="email"
 					name="email"
 					placeholder="Введите email"
 				/>
 				<input
-					{...register('password', passwordProps)}
+					{...register('password')}
 					type="password"
 					name="password"
 					placeholder="Введите пароль"
 				/>
 				<input
-					{...register('passwordCheck', passwordCheckProps)}
+					{...register('passwordCheck')}
 					type="password"
 					name="passwordCheck"
 					placeholder="Повторите пароль"
